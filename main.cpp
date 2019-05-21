98aa38069a5b8a785e65a10f3d526ca72c4700d5
chesiak <ola.chesiak@gmail.com>
2019-05-21 16:50:05 +0200

    Message

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+	int i = 0;
+	while ( i < 5 )  {
+		std::cout << "i = "<< i<< endl;
+		i++;
+	}
+	std::cout << "Welcome to GitHub" << endl;
 	std::cout << "Hello Ola" << std::endl;
-	return 1;
+	return 0;
 }
