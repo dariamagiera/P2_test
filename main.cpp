a9a31ee1da258373b1315dfd9cc02f55dd62efb0
chesiak <33420791+chesiak@users.noreply.github.com>
2019-05-21 16:51:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ int main()
 {
 	int i = 0;
 	while ( i < 5 )  {
-		std::cout << "i = "<< i<< endl;
+		std::cout << "i = "<< i<< std::endl;
 		i++;
 	}
-	std::cout << "Welcome to GitHub" << endl;
+	std::cout << "Welcome to GitHub" << std::endl;
 	std::cout << "Hello Ola" << std::endl;
 	return 0;
 }
